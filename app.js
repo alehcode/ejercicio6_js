1e38335dc95d45e5e982406d538bff7a4b846929
Alexandra Lopez <sccp.alopez@gmail.com>
2016-08-25 17:24:19 -0500

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,9 @@
+var numero;
+         
+numero=prompt("Ingrese el numero : ");
+	
+	if (parseInt(numero)%2 == 0){
+		window.alert("este numero es par");
+	} 
+	else {window.alert("este numero es inpar")}
+	
